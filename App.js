863e2615f34c043f8d531ce4e21e32093cd1d640
Matt Caswell <matt.caswell@hotmail.com>
2021-11-22 13:09:52 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View, ViewComponent } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image source={require("./assets/imdb-logo.jpeg")} style={styles.logo} />
+      </View>
+      <View>
+        <Text>Interstellar</Text>
+        <View style = {styles.filmInfo}>
+          <Text>2014</Text>
+          <Text>PG-13</Text>
+          <Text>2h 49min</Text>
+          <Text>Adventure, Drama, Sci-Fi</Text>
+          <Text>2014</Text>
+        </View>
+        <View>
+          <View><Image /></View>
+          <View>
+            <Text>A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.</Text>
+            <TouchableOpacity><Text>+ ADD TO WATCHLIST</Text></TouchableOpacity>
+          </View>
+          
+        </View>
+      </View>
+ 
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    backgroundColor: "#1D1D1D",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // paddingTop: Constants.statusBarHeight,
+    color: "#fff"
   },
+  header: {
+    height: 50,
+    backgroundColor: "#393939",
+  },
+  logo: {
+    height: 30,
+    width: 50,
+  },
+  filmInfo:{
+    flexDirection: "row"
+  }
 });
