a10a68a57f998923016bf121cf880e8a84937646
Matt Caswell <matt.caswell@hotmail.com>
2021-11-22 14:00:37 +0100

    header and description in place

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View, ViewComponent } from "react-native";
+import Constants from "expo-constants";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image source={require("./assets/imdb-logo.jpeg")} style={styles.logo} />
+        <Image source={require("./assets/imbd-logo.png")} style={styles.logo} />
       </View>
-      <View>
-        <Text>Interstellar</Text>
+      <View style={styles.filmInfoContainer}>
+        <Text style={styles.headerText}>Interstellar</Text>
         <View style = {styles.filmInfo}>
-          <Text>2014</Text>
-          <Text>PG-13</Text>
-          <Text>2h 49min</Text>
-          <Text>Adventure, Drama, Sci-Fi</Text>
-          <Text>2014</Text>
+          <Text style = {styles.filmInfoText}>2014</Text>
+          <Text style = {styles.filmInfoText}>PG-13</Text>
+          <Text style = {styles.filmInfoText}>2h 49min</Text>
+          <Text style = {styles.filmInfoText}>Adventure, Drama, Sci-Fi</Text>
         </View>
-        <View>
-          <View><Image /></View>
-          <View>
-            <Text>A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.</Text>
-            <TouchableOpacity><Text>+ ADD TO WATCHLIST</Text></TouchableOpacity>
+        <View style = {styles.filmDescContainer}>
+          <View><Image source={require("./assets/Interstellar.jpg")} style = {styles.picture} /></View>
+          <View style={styles.filmDescRight}>
+            <Text style = {styles.filmDescText}>A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.</Text>
+            <TouchableOpacity style = {styles.buttonDesign}><Text style={styles.buttonText}>+ ADD TO WATCHLIST</Text></TouchableOpacity>
           </View>
           
         </View>
@@ -34,22 +34,75 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     backgroundColor: "#1D1D1D",
     // alignItems: 'center',
     // justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    color: "#fff"
+    paddingTop: Constants.statusBarHeight,
   },
   header: {
     height: 50,
     backgroundColor: "#393939",
+    justifyContent: "center"
   },
+
   logo: {
     height: 30,
     width: 50,
+    marginLeft: 10
   },
+
+  filmInfoContainer:{
+    backgroundColor:"#212121",
+     color: "#fff"
+  },
+  
+  headerText: {
+  fontSize: 24,
+  color: "#fff",
+  margin: 10
+},
+
   filmInfo:{
-    flexDirection: "row"
+    flexDirection: "row",
+    margin: 10
+  },    
+
+  filmInfoText:{
+    color: "#fff",
+    marginRight: 10
+  },
+  picture: {
+    height:140,
+    width: 90
+  },
+
+  filmDescContainer:{
+    flexDirection: "row",
+    margin: 10,
+    justifyContent: "space-between"
+  },
+  filmDescRight:{
+    marginLeft: 10,
+    flex: 1
+  },
+  filmDescText:{
+    color: "#fff",
+   
+    flexWrap: "wrap"
+  },
+  buttonDesign:{
+    backgroundColor: "#0277BD",
+    borderRadius: 3,
+    marginTop: 10
+  },
+  buttonText:{
+    color: "#fff",
+    textAlign: "center",
+    paddingTop: 8,
+    paddingBottom: 8,
+
   }
+
+
 });
